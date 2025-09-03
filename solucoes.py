78539687241aa4b6cbb49d681ea3d0c71fe8b45c
João guilherme cordeiro <jgcreplive@gmail.com>
2025-09-03 20:48:38 -0300

    implementano a função de encontrar maior palavra

--- solucoes.py
+++ solucoes.py
@@ -6,6 +6,30 @@ def cifra_de_cesar(texto, deslocamento):
     #TODO
     pass
 
-def valida_cpf(cpf_string):
-    #TODO
-    pass
+import string
+
+def encontrar_maior_palavra(frase):
+  """
+  Encontra a maior palavra em uma frase, ignorando a pontuação.
+
+  Args:
+    frase: A string de entrada contendo a frase.
+
+  Returns:
+    A maior palavra encontrada na frase. Se houver empate, retorna a primeira.
+  """
+
+  for pontuacao in string.punctuation:
+    frase = frase.replace(pontuacao, '')
+
+  palavras = frase.split()
+
+  maior_palavra = ""
+  maior_comprimento = 0
+
+  for palavra in palavras:
+    if len(palavra) > maior_comprimento:
+      maior_comprimento = len(palavra)
+      maior_palavra = palavra
+
+  return maior_palavra

1605b96b30766784c12e58fec231c488990adbf5
oGabrie1 <gabrielconti5563@gmail.com>
2025-09-03 20:49:56 -0300

    implementado a função cifra de césar

--- solucoes.py
+++ solucoes.py
@@ -3,8 +3,17 @@ def sao_anagramas(string1, string2):
     pass
 
 def cifra_de_cesar(texto, deslocamento):
-    #TODO
-    pass
+    resultado = ''
+
+    for char in texto:
+        if char.isalpha():
+            base = ord('A') if char.isupper() else ord('a')
+            novo_char = chr((ord(char) - base + deslocamento) % 26 + base)
+            resultado += novo_char
+        else:
+            resultado += char 
+
+    return resultado
 
 def valida_cpf(cpf_string):
     #TODO

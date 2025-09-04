5c317a70415ab3e597440df1b56965fd41753939
jotage <joaoguimatz@gmail.com>
2025-09-03 21:05:30 -0300

    Alteração da função sao_anagramas

--- solucoes.py
+++ solucoes.py
@@ -6,6 +6,11 @@ def sao_anagramas(string1, string2):
     return False
 
   return sorted(s1_normalizada) == sorted(s2_normalizada)
+print(f"'amor', 'roma' -> {sao_anagramas('amor', 'roma')}")
+
+print(f"'A gentleman', 'Elegant man' -> {sao_anagramas('A gentleman', 'Elegant man')}")
+
+print(f"'gato', 'cabra' -> {sao_anagramas('gato', 'cabra')}")
 
 def cifra_de_cesar(texto, deslocamento):
     #TODO
